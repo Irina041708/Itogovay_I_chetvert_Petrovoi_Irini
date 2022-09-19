0f2adaea49aff14108956aaa08585fe2f5eb199a
Irina041708 <q-w-i-n-86@rambler.ru>
2022-09-19 08:06:57 +0400

    Zadacha_

--- Zadacha_/Program.cs
+++ Zadacha_/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Написать программу, которая из имеющего массива строк формирует массив строк, 
+длина которых меньше либо равна 3 символа. 
+1. Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма.
+2. При решении не рекомендуется пользоваться коллекциями,
+лучше обойтись исключительно массивами.
+Примеры:
+["hello","2","world",":-)"]->["2",":-)"]
+
+["1234","1567","-2","computer science"]->["-2"]
+
+["Russia","Denmark","Kazan"]->[]
+*/
+
+Console.Clear();
+
+string[] array = new string[4] { "hello", "2202", "world", ":-)" };
+
+Console.WriteLine($"Исходный массив: ");
+Console.Write($" x = [");
+PrintArray(array);
+Console.WriteLine($"]");
+
+Console.WriteLine("Массив строк,длина которых меньше либо равна 3 символа: ");
+Console.Write($" y = [");
+PrintArray2(array);
+Console.Write($"]");
+
+void PrintArray(string[] arrayy)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(" '{1}'  ", i, arrayy[i]);
+    }
+}
+
+void PrintArray2(string[] arrayy)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(" '{1}'  ", i, arrayy[i]);
+        }
+    }
+}
